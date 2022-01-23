29c5d20f14d186263d225a85bd74e4cb8c98082d
ftom789 <ftom789@gmail.com>
2022-01-23 23:15:23 +0200

    add usb detection

--- nullFilter.c
+++ nullFilter.c
@@ -1,15 +1,133 @@
 #include <fltKernel.h>
 #include <dontuse.h>
 #include <suppress.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Ntddvol.h>
+
+
+#define USB_NAME L"USBSTOR\\DiskSanDisk_Cruzer_Blade____1.00"
+//#define IOCTL_VOLUME_BASE   ((DWORD32) 'V')
+
+//#define IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS    CTL_CODE(IOCTL_VOLUME_BASE, 0, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+
+#define DFDBG_TRACE_ERRORS              0x00000001
+#define DFDBG_TRACE_ROUTINES            0x00000002
+#define DFDBG_TRACE_OPERATION_STATUS    0x00000004
+
+#define DF_VOLUME_GUID_NAME_SIZE        48
+
+#define DF_INSTANCE_CONTEXT_POOL_TAG    'nIfD'
+#define DF_STREAM_CONTEXT_POOL_TAG      'xSfD'
+#define DF_TRANSACTION_CONTEXT_POOL_TAG 'xTfD'
+#define DF_ERESOURCE_POOL_TAG           'sRfD'
+#define DF_DELETE_NOTIFY_POOL_TAG       'nDfD'
+#define DF_STRING_POOL_TAG              'rSfD'
+
+#define DF_CONTEXT_POOL_TYPE            PagedPool
+
+#define DF_NOTIFICATION_MASK            (TRANSACTION_NOTIFY_COMMIT_FINALIZE | \
+                                         TRANSACTION_NOTIFY_ROLLBACK)
+
+NTSTATUS CopyUnicodeStrings(UNICODE_STRING dest, UNICODE_STRING source)
+{
+    dest.Length = source.Length;
+    dest.MaximumLength = source.MaximumLength;
+    dest.Buffer=(PWCHAR)ExAllocatePool(NonPagedPool, source.MaximumLength);
+    if (dest.Buffer != NULL)
+    {
+        RtlZeroMemory(dest.Buffer, source.MaximumLength);
+        RtlCopyMemory(dest.Buffer, source.Buffer, dest.MaximumLength);
+        return STATUS_BUFFER_ALL_ZEROS;
+    }
+    return STATUS_SUCCESS;
+
+}
+
+NTSTATUS
+DfAllocateUnicodeString(
+    _Inout_ PUNICODE_STRING String
+)
+/*++
+Routine Description:
+    This helper routine simply allocates a buffer for a UNICODE_STRING and
+    initializes its Length to zero.
+    It uses whatever value is present in the MaximumLength field as the size
+    for the allocation.
+Arguments:
+    String - Pointer to UNICODE_STRING.
+Return Value:
+    STATUS_INSUFFICIENT_RESOURCES if it was not possible to allocate the
+    buffer from pool.
+    STATUS_SUCCESS otherwise.
+--*/
+{
+    PAGED_CODE();
+
+    ASSERT(NULL != String);
+    ASSERT(0 != String->MaximumLength);
+
+    String->Length = 0;
+
+    String->Buffer = ExAllocatePoolWithTag(DF_CONTEXT_POOL_TYPE,
+        String->MaximumLength,
+        DF_STRING_POOL_TAG);
+
+    if (NULL == String->Buffer) {
+
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    return STATUS_SUCCESS;
+}
+
+VOID
+DfFreeUnicodeString(
+    _Inout_ PUNICODE_STRING String
+)
+/*++
+Routine Description:
+    This helper routine frees the buffer of a UNICODE_STRING and resets its
+    Length to zero.
+Arguments:
+    String - Pointer to UNICODE_STRING.
+--*/
+{
+    PAGED_CODE();
+
+    ASSERT(NULL != String);
+    ASSERT(0 != String->MaximumLength);
+
+    String->Length = 0;
+
+    if (NULL != String->Buffer) {
+
+        String->MaximumLength = 0;
+        ExFreePool(String->Buffer);
+        String->Buffer = NULL;
+    }
+}
 
 //https://github.com/microsoft/Windows-driver-samples/tree/master/filesys/miniFilter/nullFilter
+//https://community.osr.com/discussion/237575/minifilter-directly-changing-irp-mj-write-buffers
+
+
 
 PFLT_FILTER FilterHandle = NULL;
+NTSTATUS AddDevice(__in struct _DRIVER_OBJECT* DriverObject, __in struct _DEVICE_OBJECT* PhysicalDeviceObject);
 NTSTATUS MiniUnload(FLT_FILTER_UNLOAD_FLAGS Flags);
 FLT_POSTOP_CALLBACK_STATUS MiniPostCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags);
 FLT_PREOP_CALLBACK_STATUS MiniPreCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext);
 FLT_PREOP_CALLBACK_STATUS MiniPreWrite(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext);
 FLT_PREOP_CALLBACK_STATUS MiniPostRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext);
+NTSTATUS PfltInstanceSetupCallback(
+    __in PCFLT_RELATED_OBJECTS FltObjects,
+    __in FLT_INSTANCE_SETUP_FLAGS Flags,
+    __in DEVICE_TYPE VolumeDeviceType,
+    __in FLT_FILESYSTEM_TYPE VolumeFilesystemType
+);
 
 const FLT_OPERATION_REGISTRATION Callbacks[] = {
  {IRP_MJ_CREATE, 0, MiniPreCreate, MiniPostCreate},
@@ -25,7 +143,7 @@ const FLT_REGISTRATION FilterRegistration = {
  NULL,
  Callbacks,
  MiniUnload,
- NULL,
+ PfltInstanceSetupCallback,
  NULL,
  NULL,
  NULL,
@@ -35,9 +153,291 @@ const FLT_REGISTRATION FilterRegistration = {
  NULL
 };
 
+
+
+typedef struct {
+    PFLT_VOLUME Volume;
+    UNICODE_STRING GUID;
+    PFLT_VOLUME_PROPERTIES VolumeProperties;
+} Volume,*PVolume;
+
+PVolume v = NULL;
+
+_Use_decl_annotations_
+NTSTATUS
+MyAddDevice(
+    struct _DRIVER_OBJECT* DriverObject,
+    struct _DEVICE_OBJECT* PhysicalDeviceObject
+)
+{
+    KdPrint(("adddevice call"));
+    return STATUS_SUCCESS;
+}
+
+
+
+
+NTSTATUS PfltInstanceSetupCallback(
+    __in PCFLT_RELATED_OBJECTS FltObjects,
+    __in FLT_INSTANCE_SETUP_FLAGS Flags,
+    __in DEVICE_TYPE VolumeDeviceType,
+    __in FLT_FILESYSTEM_TYPE VolumeFilesystemType
+)
+{
+    
+
+    NTSTATUS status;
+    //KdPrint(("Volume"));
+    
+    KdPrint(("-------------\n"));
+    //v->Volume = FltObjects->Volume;
+    
+    KdPrint(("Print Volume\n"));
+    
+    ULONG BufferSize;
+    status = FltGetVolumeGuidName(FltObjects->Volume, NULL, &BufferSize);
+    
+    UNICODE_STRING tempString;
+    tempString.MaximumLength = BufferSize *
+        sizeof(WCHAR) +
+        sizeof(WCHAR);
+
+    
+    
+    
+    status = DfAllocateUnicodeString(&tempString);
+    
+
+    status = FltGetVolumeGuidName(FltObjects->Volume, &tempString, &BufferSize);
+
+    if (status == STATUS_BUFFER_TOO_SMALL)
+    {
+        KdPrint(("Buffer too small\n"));
+    }
+    else if (status == STATUS_INSUFFICIENT_RESOURCES)
+    {
+        KdPrint(("STATUS_INSUFFICIENT_RESOURCES\n"));
+    }
+    else if (status == STATUS_INVALID_DEVICE_REQUEST)
+    {
+        KdPrint(("STATUS_INVALID_DEVICE_REQUEST\n"));
+    }
+    else if (status == STATUS_FLT_VOLUME_NOT_FOUND)
+    {
+        KdPrint(("STATUS_FLT_VOLUME_NOT_FOUND\n"));
+    }
+    CopyUnicodeStrings(v->GUID, tempString);
+    KdPrint(("guid: %wZ\n", tempString));
+    DfFreeUnicodeString(&tempString);
+    
+
+
+
+    PFLT_VOLUME_PROPERTIES volumeProperties=(PFLT_VOLUME_PROPERTIES)ExAllocatePool(NonPagedPool, (sizeof(FLT_VOLUME_PROPERTIES) + 512));
+    ULONG volumePropertiesLength;
+    
+    status = FltGetVolumeProperties(FltObjects->Volume,
+        volumeProperties,
+        (sizeof(FLT_VOLUME_PROPERTIES) + 512) ,
+        &volumePropertiesLength);
+    if (status== STATUS_BUFFER_OVERFLOW)
+    {
+        KdPrint(("STATUS_BUFFER_OVERFLOW\n"));
+    }
+    else if (status == STATUS_BUFFER_TOO_SMALL)
+    {
+        KdPrint(("STATUS_BUFFER_TOO_SMALL\n"));
+    }
+    else {
+        KdPrint(("Success DeviceName Properties %wZ\n", volumeProperties->FileSystemDeviceName));
+        KdPrint(("Success RealDeviceName Properties %wZ\n",volumeProperties->RealDeviceName));
+        KdPrint(("Success DriverName Properties %wZ\n", volumeProperties->FileSystemDriverName));
+        
+    }
+    KdPrint(("->->->->->\n"));
+    
+    PDEVICE_OBJECT deviceObj = NULL;
+    status = FltGetDiskDeviceObject(FltObjects->Volume, &deviceObj);
+
+
+    if (!NT_SUCCESS(status)||deviceObj==NULL)
+    {
+        KdPrint(("failed deviceObj\n"));
+        return STATUS_FLT_DO_NOT_ATTACH;
+    }
+
+    /*
+    PDEVICE_OBJECT DeviceObj=NULL;
+    PFILE_OBJECT fileObj = NULL;
+    
+    
+    UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\??\\PhysicalDrive1");
+    status=IoGetDeviceObjectPointer(&DeviceName,FILE_ALL_ACCESS,&fileObj,&DeviceObj);
+    if (!NT_SUCCESS(status))
+    {
+        KdPrint(("failed get deviceObj"));
+        return STATUS_SUCCESS;
+    }
+    PDEVICE_OBJECT PhyDeviceObj = NULL;
+    PhyDeviceObj = IoGetDeviceAttachmentBaseRef(DeviceObj);
+    if (PhyDeviceObj == NULL)
+    {
+        KdPrint(("failed get PhysicDeviceObj"));
+    }
+    
+    int size = 32;
+    PWCHAR deviceID = (PWCHAR)ExAllocatePool(NonPagedPool, size);
+    ULONG dataLen;
+    RtlZeroMemory(deviceID, size);
+    status = IoGetDeviceProperty(PhyDeviceObj, DevicePropertyHardwareID, size, deviceID, &dataLen);
+    
+    while (status == STATUS_BUFFER_TOO_SMALL)
+    {
+       
+            
+        ExFreePool(deviceID);
+        size *= 2;
+        deviceID = (PWCHAR)ExAllocatePool(NonPagedPool, size);
+        RtlZeroMemory(deviceID, size);
+        status = IoGetDeviceProperty(PhyDeviceObj, DevicePropertyHardwareID, size, deviceID, &dataLen);
+        if (status == STATUS_BUFFER_TOO_SMALL)
+        {
+            KdPrint(("STATUS_BUFFER_TOO_SMALL\n"));
+        }
+        else if (status == STATUS_INVALID_PARAMETER_2)
+        {
+            KdPrint(("STATUS_INVALID_PARAMETER_2\n"));
+        }
+        else if (status == STATUS_INVALID_DEVICE_REQUEST)
+        {
+            KdPrint(("STATUS_INVALID_DEVICE_REQUEST\n"));
+        }
+        
+        
+        
+    }
+    
+    if (status == STATUS_BUFFER_TOO_SMALL)
+    {
+        KdPrint(("STATUS_BUFFER_TOO_SMALL\n"));
+    }
+    else if (status == STATUS_INVALID_PARAMETER_2)
+    {
+        KdPrint(("STATUS_INVALID_PARAMETER_2\n"));
+    }
+    else if (status == STATUS_INVALID_DEVICE_REQUEST)
+    {
+        KdPrint(("STATUS_INVALID_DEVICE_REQUEST\n"));
+    }
+    else if (status==STATUS_SUCCESS) {
+        KdPrint(("STATUS_SUCCESS\n"));
+        size = wcslen(deviceID);
+        KdPrint(("deviceId: %d\n", size));
+    }
+    else
+    {
+        KdPrint(("error code %x", status));
+    }
+    
+    ExFreePool(deviceID);
+    //v->VolumeProperties = &volumeProperties;
+    
+    */
+    /**/
+
+    //https://community.osr.com/discussion/comment/178370#Comment_178370
+
+
+
+    KEVENT WaitEvent;
+    PIRP NewIrp;
+    NTSTATUS status1;
+    PDEVICE_OBJECT ndevice;
+    PDEVICE_OBJECT device;
+    PFILE_OBJECT file;
+    ULONG lenght;
+    WCHAR hwId[512];
+    PVOLUME_DISK_EXTENTS volumeextents;
+    PUNICODE_STRING devicename;
+    wchar_t data[25];
+    IO_STATUS_BLOCK IoStatus = { 0 };
+    
+    KeInitializeEvent(&WaitEvent, NotificationEvent, FALSE);
+
+    volumeextents = (PVOLUME_DISK_EXTENTS)ExAllocatePool(NonPagedPool, sizeof(VOLUME_DISK_EXTENTS));
+
+
+    //PDEVICE_OBJECT lowerDeviceObj = IoGetLowerDeviceObject(deviceObj);
+    NewIrp = IoBuildDeviceIoControlRequest(IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS,
+        deviceObj,
+        NULL, 0,
+        (PVOID)volumeextents, sizeof(VOLUME_DISK_EXTENTS),
+        FALSE, &WaitEvent, &IoStatus);
+    KdPrint(("status 0x%x \n", IoStatus.Status));
+
+    if (!NewIrp) {
+        DbgPrint("Failed to create new IRP,IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS");
+        ExFreePool(volumeextents);
+        return;
+    }
+
+    // send this irp to the storage device
+    PIO_STACK_LOCATION irpnext = NULL;
+    irpnext = IoGetNextIrpStackLocation(NewIrp);
+    DbgPrint("Setze jetz IoCall ab!\n");
+    
+    status = IoCallDriver(deviceObj, NewIrp);
+    
+    if (status == STATUS_PENDING) {
+        status = KeWaitForSingleObject(&WaitEvent, Executive, KernelMode, FALSE,
+            NULL);
+        status = IoStatus.Status;
+    }
+
+    if (!NT_SUCCESS(status)) {
+        DbgPrint("IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS failed, status =0x%x\n", status);
+        //Flt
+        return STATUS_FLT_DO_NOT_ATTACH;
+    }
+    
+    wcscpy(data, L"\\GLOBAL??\\PhysicalDrive");
+    KdPrint(("copy string to data2\n"));
+    KdPrint(("status 0x%x \n", IoStatus.Status));
+    KdPrint(("exte %d \n", volumeextents->NumberOfDiskExtents));
+    data[23] = volumeextents->Extents[0].DiskNumber + 0x30;
+    data[24] = 0;
+
+    RtlInitUnicodeString(&devicename, data);
+    KdPrint(("Init string\n"));
+    IoGetDeviceObjectPointer(&devicename, FILE_READ_ATTRIBUTES, &file, &device);
+    ndevice = IoGetDeviceAttachmentBaseRef(device);
+    KdPrint(("GotDevice obj\n"));
+    status1 = IoGetDeviceProperty(ndevice, DevicePropertyHardwareID, sizeof(hwId), hwId, &lenght);
+    KdPrint(("GotDevice Prop\n"));
+
+    if (!NT_SUCCESS(status1)) {
+        DbgPrint(" failed, status =0x%x\n", status1);
+        
+    }
+    else
+    {
+        KdPrint(("the hwid is: %ls", hwId));
+        if (wcscmp(USB_NAME, hwId) == 0)
+        {
+            KdPrint(("identical\n"));
+            return STATUS_SUCCESS;
+        }
+    }
+    KdPrint(("-----------------\n"));
+    /**/
+    return STATUS_FLT_DO_NOT_ATTACH;
+}
+
+
 NTSTATUS MiniUnload(FLT_FILTER_UNLOAD_FLAGS Flags)
 {
     KdPrint(("Driver unloaded2 \r\n"));
+    ExFreePool(v);
     FltUnregisterFilter(FilterHandle);
 
     return STATUS_SUCCESS;
@@ -46,6 +446,7 @@ NTSTATUS MiniUnload(FLT_FILTER_UNLOAD_FLAGS Flags)
 FLT_POSTOP_CALLBACK_STATUS MiniPostCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags)
 {
     //KdPrint(("Post create is running \r\n"));
+    
     return FLT_POSTOP_FINISHED_PROCESSING;
 }
 
@@ -116,10 +517,10 @@ FLT_PREOP_CALLBACK_STATUS MiniPreWrite(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OB
 
                     if (ioBuffer != NULL)
                     {
-                        KdPrint(("the write buffer is: %s", ioBuffer));
+                        KdPrint(("the write buffer is: %s\n", ioBuffer));
                     }
                     else {
-                        KdPrint(("the write buffer is null"));
+                        KdPrint(("the write buffer is null\n"));
                     }
 
 
@@ -198,6 +599,7 @@ FLT_PREOP_CALLBACK_STATUS MiniPostRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OB
                     PMDL MdlAddress = Data->Iopb->Parameters.Read.MdlAddress;
                     ULONG Length = (ULONG)Data->Iopb->Parameters.Read.Length;
                     
+
                     if (NULL != MdlAddress)
                     {
                         // Don't expect chained MDLs this high up the stack
@@ -234,19 +636,107 @@ FLT_PREOP_CALLBACK_STATUS MiniPostRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OB
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
     NTSTATUS status;
-
+    DRIVER_ADD_DEVICE MyAddDevice;
+    DriverObject->DriverExtension->AddDevice = MyAddDevice;
+    
     status = FltRegisterFilter(DriverObject, &FilterRegistration, &FilterHandle);
 
     if (NT_SUCCESS(status))
     {
+       
+        v = (PVolume)ExAllocatePool(NonPagedPool, sizeof(Volume)); //allocate memory for Volume struct
+        //IoGetDeviceObjectPointer
+        //IoGetDeviceAttachmentBaseRef
 
         status = FltStartFiltering(FilterHandle);
-
         if (!NT_SUCCESS(status))
         {
             FltUnregisterFilter(FilterHandle);
         }
+
+        
+        //char VolumeName[30] = "\\Device\\HarddiskVolume";
+        
+        
+        
+        /*
+        BOOLEAN Found = FALSE;
+        int i = 1;
+
+        while (!Found&&i!=2)
+        {
+            if (v != NULL)
+            {
+                strcpy(VolumeName, "\\Device\\HarddiskVolume");
+                RtlZeroMemory(v, sizeof(Volume));
+
+                char num[4];
+
+                itoa(i, &num, 10);
+
+                strcat(VolumeName, num);
+                wchar_t  ws[100];
+                mbstowcs(ws, VolumeName, 100);
+
+                UNICODE_STRING str = RTL_CONSTANT_STRING(ws);
+
+                KdPrint(("%wZ \n", str));
+
+                CopyUnicodeStrings(v->VolumeName, str);
+                
+                //KdPrint(("%wZ \n", v->VolumeName));
+                v->Volume = NULL;
+                UNICODE_STRING str2 = RTL_CONSTANT_STRING(L"\\Device\\HarddiskVolume2");
+                str.Length = str2.Length;
+                status=FltGetVolumeFromName(FilterHandle, &str2,&(v->Volume));
+                
+                if (!NT_SUCCESS(status))
+                {
+                    KdPrint(("failed get Pflt Volume\n"));
+                    return status;
+                }
+                
+                
+
+                ULONG BufferSize;
+                status = FltGetVolumeGuidName(v->Volume, NULL, &BufferSize);
+
+                UNICODE_STRING tempString;
+                tempString.MaximumLength = BufferSize *
+                    sizeof(WCHAR) +
+                    sizeof(WCHAR);
+
+                status = DfAllocateUnicodeString(&tempString);
+
+
+                status = FltGetVolumeGuidName(v->Volume, &tempString, &BufferSize);
+
+                if (!NT_SUCCESS(status))
+                {
+                    KdPrint(("failed get GUID\n"));
+                    return status;
+                }
+                KdPrint(("guid: %wZ", tempString));
+                DfFreeUnicodeString(&tempString);
+                
+                */
+                /*
+                
+                UNICODE_STRING guidNeed = RTL_CONSTANT_STRING(L"\\??\\Volume{4d36e967-e325-11ce-bfc1-08002be10318}");
+                KdPrint(("guid - %wZ", &v->GUID));
+                if ((RtlCompareUnicodeString(&guidNeed,&v->GUID,TRUE)))
+                {
+                    break;
+                }
+
+                
+            }
+            i++;
+        }*/
+        
+
+        
     }
 
-    return status;
+    return STATUS_SUCCESS;
 }
